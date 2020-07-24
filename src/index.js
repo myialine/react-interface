dade0033c1ba922ad9581c291ffa085ce1e2aeaa
Aline Cavalcante <Aline-Cavalcante@live.nmit.ac.nz>
2020-07-24 16:39:32 +1200

    changes in library

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/bootstrap.css';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import 'jquery/dist/jquery'
 
 ReactDOM.render(
   <React.StrictMode>
